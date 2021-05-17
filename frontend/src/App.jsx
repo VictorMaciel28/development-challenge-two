05d356e25d5d195ff10e3ee114b86054476f2aba
Victor Marques <victor.silva@tg4.com.br>
2021-05-17 06:47:19 -0300

    Melhora experiência e refatora código

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
 import './App.css';
-import React, { useRef, useEffect, useState, useCallback } from "react";
-import {Grid, Toolbar, Button} from '@material-ui/core';
+import React, { useEffect, useState, useCallback } from "react";
+import { Grid, Toolbar, Button } from '@material-ui/core';
 import Modal from './components/Modal';
 import UserContent from './components/UserContent';
 import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+  button: {
+    textAlign: 'center',
+    margin: '5%'
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   const [users, setUsers] = useState([]);
-
   const [formNameUser, setFormNameuser] = useState([]);
   const [formAge, setFormAge] = useState([]);
   const [formSymptom, setformSymptom] = useState([]);
   const [formDiagnostic, setformDiagnostic] = useState([]);
   const [formMedications, setformMedications] = useState([]);
   const [formRecommendations, setformRecommendations] = useState([]);
-
+  const [openModal, setOpenModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   const getUsers = useCallback(async () => {
@@ -25,17 +30,16 @@ function App() {
     const response = await fetch(`https://43a45gdrtj.execute-api.us-east-2.amazonaws.com/getUsers`);
     const data = await response.json();
     const result = data.Items;
-
     setUsers(result)
     setIsLoading(false);
     return result;
-  },[]);
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     getUsers();
-  },[]);
+  }, []);
 
-  const formSubmissionHandler = event =>{
+  const formSubmissionHandler = event => {
     event.preventDefault();
   }
   const handleNameUserChange = event => {
@@ -56,44 +60,70 @@ function App() {
   const handleformRecommendations = event => {
     setformRecommendations(event.target.value)
   }
- 
-  const submitForm = () => {
-    fetch('https://lh92scf3o8.execute-api.us-east-2.amazonaws.com/putUser', {
-      method:'POST',
+  const handleOpenModal = () => {
+    setOpenModal(true)
+  }
+  const handleCloseModal = () => {
+    setOpenModal(false)
+  }
+
+  async function submitForm() {
+    const response = await fetch('https://lh92scf3o8.execute-api.us-east-2.amazonaws.com/putUser', {
+      method: 'POST',
       body: JSON.stringify({
-        //"age": "" + formAge + "",
-        "name": `'formNameUser'`
-        //"symptom": formSymptom,
-        //"diagnostic": formDiagnostic,
-        //"medications": formMedications,
-        //"recommendations": formRecommendations
+        age: formAge,
+        name: formNameUser,
+        symptom: formSymptom,
+        diagnostic: formDiagnostic,
+        medications: formMedications,
+        recommendations: formRecommendations
       }),
-      contentType:"application/json"
+      'Content-Type': "application/json",
+    });
+    if (response.ok) {
+      console.log(await response.json());
+      alert("Enviado com sucesso!");
+    } else {
+      console.log(await response.json());
+      alert("Houve algum problema no envio.")
     }
-  )}
+    handleCloseModal();
+    getUsers();
+  }
+
   const modalForm = (
-      <form onSubmit={formSubmissionHandler}>
-        <h3>Preencha as informações</h3>
-        <TextField fullWidth onChange={handleNameUserChange}  label="Nome e Sobrenome" required />
-        <TextField fullWidth onChange={handleAgeChange} label="Idade" />
-        <TextField fullWidth onChange={handleformSymptomChange} label="Sintomas"/>
-        <TextField fullWidth onChange={handleformDiagnosticChange} label="Diagnóstico"/>
-        <TextField fullWidth onChange={handleformMedicationsChange} label="Medicacoes"/>
-        <TextField fullWidth onChange={handleformRecommendations} label="Recomendacoes"/>
-        <button onClick={submitForm}>Enviar</button>
-      </form>
+    <form onSubmit={formSubmissionHandler}>
+      <h3>Preencha as informações</h3>
+      <TextField fullWidth onChange={handleNameUserChange} label="Nome e Sobrenome" required />
+      <TextField fullWidth onChange={handleAgeChange} label="Idade" />
+      <TextField fullWidth onChange={handleformSymptomChange} label="Sintomas" />
+      <TextField fullWidth onChange={handleformDiagnosticChange} label="Diagnóstico" />
+      <TextField fullWidth onChange={handleformMedicationsChange} label="Medicacoes" />
+      <TextField fullWidth onChange={handleformRecommendations} label="Recomendacoes" />
+      <Grid container>
+        <Button item xs={6} className={classes.button} onClick={submitForm} variant="contained" color="primary" disableElevation>
+          Adicionar cliente
+        </Button>
+        <Button item xs={6} className={classes.button} onClick={handleCloseModal} variant="contained" color="secondary" disableElevation>
+          Cancelar
+        </Button>
+      </Grid>
+    </form>
   );
+
+  
 
   return (
     <div>
       <Toolbar className="app-header">Lista de informação de clientes</Toolbar>
       <Grid container className="container" justify="center">
         {isLoading ? <p>Um momentinho...</p> :
-        users ? <UserContent content={users}></UserContent> :
-        <p>Insira algum usuário</p>}
+          users ? <UserContent content={users}></UserContent> :
+            <p>Insira algum usuário</p>}
       </Grid>
       <div className="button-session">
-        <Modal value="Adicionar cliente" content={modalForm}></Modal>
+        <Button variant="contained" onClick={handleOpenModal}  color="primary">Adicionar Cliente</Button>
+        <Modal value="Adicionar cliente" content={modalForm} handleClose={handleCloseModal} conditionModal={openModal} ></Modal>
       </div>
       <div className="footer"></div>
     </div>
--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import {Button} from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
@@ -27,39 +25,16 @@ function getModalStyle() {
 export default function SimpleModal(props) {
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-      
-    </div>
-  );
-
   return (
     <div>
-      <Button variant ="contained" onClick={handleOpen} color="primary">{props.value}</Button>
       <Modal
-        open={open}
-        onClose={handleClose}
+        open={props.conditionModal}
+        onClose={props.handleClose}
         aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
+        aria-describedby="simple-modal-description">
         <div style={modalStyle} className={classes.paper}>
-        {props.content}
+          {props.content}
         </div>
-        
       </Modal>
     </div>
   );
